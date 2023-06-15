d652ef06956829e08fc546c3e9c16aecc31a833c
Nchien <nguyenchienkts96@gmail.com>
2023-06-15 23:17:37 +0700

    v3

--- test/TestToken.js
+++ test/TestToken.js
@@ -1,14 +1,84 @@
 const { expect } = require("chai");
 
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+
 describe("Token contract", function () {
-  it("Deployment should assign the total supply of tokens to the owner", async function () {
-    const [owner] = await ethers.getSigners();
+  async function deployTokenFixture() {
+    const [owner, addr1, addr2] = await ethers.getSigners();
 
-    const hardhatToken = await ethers.deployContract("CarCrypto");
+    const CarCryptoToken = await ethers.deployContract("CarCrypto");
 
-    const ownerBalance = await hardhatToken.balanceOf(owner.address);
-    expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    await CarCryptoToken.waitForDeployment();
+
+    return { CarCryptoToken, owner, addr1, addr2 };
+  }
+
+  describe("Deployment", function () {
+    it("Should set the right owner", async function () {
+      const { CarCryptoToken, owner } = await loadFixture(deployTokenFixture);
+
+      expect(await CarCryptoToken.owner()).to.equal(owner.address);
+    });
+
+    it("Should assign the total supply of tokens to the owner", async function () {
+      const { CarCryptoToken, owner } = await loadFixture(deployTokenFixture);
+      const ownerBalance = await CarCryptoToken.balanceOf(owner.address);
+      expect(await CarCryptoToken.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+
+  describe("Transactions", function () {
+    it("Should transfer tokens between accounts", async function () {
+      const { CarCryptoToken, owner, addr1, addr2 } = await loadFixture(
+        deployTokenFixture
+      );
+      // Transfer 50 tokens from owner to addr1
+      await expect(
+        CarCryptoToken.transfer(addr1.address, 50)
+      ).to.changeTokenBalances(CarCryptoToken, [owner, addr1], [-50, 50]);
+
+      // Transfer 50 tokens from addr1 to addr2
+      // We use .connect(signer) to send a transaction from another account
+      await expect(
+        CarCryptoToken.connect(addr1).transfer(addr2.address, 50)
+      ).to.changeTokenBalances(CarCryptoToken, [addr1, addr2], [-50, 50]);
+    });
+
+    it("Should emit Transfer events", async function () {
+      const { CarCryptoToken, owner, addr1, addr2 } = await loadFixture(
+        deployTokenFixture
+      );
+
+      // Transfer 50 tokens from owner to addr1
+      await expect(CarCryptoToken.transfer(addr1.address, 50))
+        .to.emit(CarCryptoToken, "Transfer")
+        .withArgs(owner.address, addr1.address, 50);
+
+      // Transfer 50 tokens from addr1 to addr2
+      // We use .connect(signer) to send a transaction from another account
+      await expect(CarCryptoToken.connect(addr1).transfer(addr2.address, 50))
+        .to.emit(CarCryptoToken, "Transfer")
+        .withArgs(addr1.address, addr2.address, 50);
+    });
+
+    it("Should fail if sender doesn't have enough tokens", async function () {
+      const { CarCryptoToken, owner, addr1 } = await loadFixture(
+        deployTokenFixture
+      );
+      const initialOwnerBalance = await CarCryptoToken.balanceOf(owner.address);
+
+      // Try to send 1 token from addr1 (0 tokens) to owner.
+      // `require` will evaluate false and revert the transaction.
+      await expect(
+        CarCryptoToken.connect(addr1).transfer(owner.address, 1)
+      ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
+
+      // Owner balance shouldn't have changed.
+      expect(await CarCryptoToken.balanceOf(owner.address)).to.equal(
+        initialOwnerBalance
+      );
+    });
   });
 });
-
-ChannelSplitterNode;
